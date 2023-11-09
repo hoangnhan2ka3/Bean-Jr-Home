53a9f399787748438ed2f710cc12469bcbd87526
hoangnhan2ka3 <workwith.hnhan@gmail.com>
2023-11-09 21:07:03 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -24,5 +24,50 @@ window.addEventListener("scroll", function(){
    });
 });
 
+window.addEventListener("scroll", function(){
+   const sections = Array.from(document.querySelectorAll(".section"));
+   const menuScrolls = document.querySelectorAll(".menu-scroll");
 
+   // Xóa lớp "active" khỏi tất cả các thẻ
+   menuScrolls.forEach(function(menuScroll) {
+      menuScroll.classList.remove('active');
+   });
 
+   // Nếu người dùng đang ở đầu trang, thêm lớp "active" vào liên kết "Trang chủ"
+   if (window.scrollY === 0) {
+      const homeLink = document.querySelector('.home-link');
+      homeLink.classList.add('active');
+      return;
+   }
+
+   let maxVisible = 0;
+   let mostVisibleSection = null;
+
+   for (let section of sections) {
+      const rect = section.getBoundingClientRect();
+      const visibleHeight = Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0);
+      if (visibleHeight > maxVisible) {
+         maxVisible = visibleHeight;
+         mostVisibleSection = section;
+      }
+   }
+
+   if (mostVisibleSection) {
+      const id = mostVisibleSection.getAttribute('id');
+      const link = document.querySelector(`.menu-scroll[href="#${id}"]`);
+      if (link) {
+         link.classList.add('active');
+      }
+   }
+});
+
+window.onload = function() {
+   if(window.location.href !== "https://hoangnhan2ka3.github.io/Bean-Jr-Home/") {
+      window.location.href = "https://hoangnhan2ka3.github.io/Bean-Jr-Home/";
+   }
+}
+
+window.onload = function() {
+   const homeLink = document.querySelector('.home-link');
+   homeLink.classList.add('active');
+}
